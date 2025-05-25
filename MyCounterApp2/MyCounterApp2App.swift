efea48a3985c78524d99623acca48298c9ddf294
sunyuan <sunyuan@aspirecn.com>
2025-05-25 18:24:16 +0800

    Initial Commit

---
+++ MyCounterApp2/MyCounterApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyCounterApp2App.swift
+//  MyCounterApp2
+//
+//  Created by sunyuan on 2025/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MyCounterApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
